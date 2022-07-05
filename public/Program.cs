ff88485134f738a9d35ddcb088b6394bd853b42f
Nata <Beloborodova.n.s@bk.ru>
2022-07-06 00:58:34 +0300

    dz2

---
+++ public/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2: 
+
+int a, b; 
+Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine("max = a");
+}
+else if (a<b)
+{
+    Console.WriteLine("max = b");
+}
